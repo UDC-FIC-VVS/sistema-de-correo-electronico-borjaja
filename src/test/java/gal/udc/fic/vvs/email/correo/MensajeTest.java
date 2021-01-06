f04f92dd800bac7e4a4449df7603e999853d3a40
Borja <borja.alvarez.pineiro@udc.es>
2021-01-06 15:15:22 +0100

    marca de lectura, marca que debe ser editable

--- src/test/java/gal/udc/fic/vvs/email/correo/MensajeTest.java
+++ src/test/java/gal/udc/fic/vvs/email/correo/MensajeTest.java
@@ -1,5 +1,7 @@
 package gal.udc.fic.vvs.email.correo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import gal.udc.fic.vvs.email.archivo.Texto;
@@ -15,4 +17,15 @@ public class MensajeTest {
 
 	}
 
+	@Test
+	public void TestMensajeLeidoStateIsEditable() {
+
+		Texto text = new Texto("Text name", "Text content");
+		Mensaje msg = new Mensaje(text);
+		msg.establecerLeido(true);
+		assertEquals(0, msg.obtenerNoLeidos());
+		msg.establecerLeido(false);
+		assertEquals(1, msg.obtenerNoLeidos());
+
+	}
 }
